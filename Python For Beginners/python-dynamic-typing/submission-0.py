0df6a6143d4af4e5dcc80a3f2d7e50266c0a8924
spananji <121888648+spananji@users.noreply.github.com>
2026-05-28 22:58:41 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 6.7
+print(type(variable))
+variable = True
+print(type(variable))
+variable = 'Emma'
+print(type(variable))
+variable = ['shiju', 'sam']
+print(type(variable))
